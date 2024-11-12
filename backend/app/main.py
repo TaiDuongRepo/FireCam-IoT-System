8c7cd878c107cd5905754bfabdee7b86b19d6047
TaiDuongRepo <taiduong1301@gmail.com>
2024-11-13 00:19:35 +0700

    Update backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.middleware.cors import CORSMiddleware
 
+from fastapi.responses import StreamingResponse
+from fastapi.templating import Jinja2Templates
+
+from models.detection import Detection
+import cv2
 
 app = FastAPI()
+detection = Detection()
 
 
 origins = ["*"]
@@ -13,6 +19,33 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# async def read_root():
+#     return {"Hello": "World"}
+
+
+def gen_frames():
+    cap = cv2.VideoCapture(0)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, (512, 512))
+        if frame is None:
+            break
+        frame = detection.detect_from_image(frame)
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+templates = Jinja2Templates(directory="/code/app/templates")
+@app.get('/')
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+
+@app.get('/video_feed')
+def video_feed():
+    return StreamingResponse(gen_frames(), media_type='multipart/x-mixed-replace; boundary=frame')
---
+++ backend/app/models/detection.py
@@ -0,0 +1,30 @@
+from ultralytics import YOLO
+import cv2
+import time
+
+class Detection:
+    def __init__(self):
+        #download weights from here:https://github.com/ultralytics/ultralytics and change the path
+        self.model = YOLO(r"app/best.pt")
+
+    def predict(self, img, classes=[], conf=0.5):
+        if classes:
+            results = self.model.predict(img, classes=classes, conf=conf)
+        else:
+            results = self.model.predict(img, conf=conf)
+        return results
+
+    def predict_and_detect(self, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
+        results = self.predict(img, classes, conf=conf)
+        for result in results:
+            for box in result.boxes:
+                cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                              (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), rectangle_thickness)
+                cv2.putText(img, f"{result.names[int(box.cls[0])]}",
+                            (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                            cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
+        return img, results
+
+    def detect_from_image(self, image):
+        result_img, _ = self.predict_and_detect(image, classes=[], conf=0.5)
+        return result_img
